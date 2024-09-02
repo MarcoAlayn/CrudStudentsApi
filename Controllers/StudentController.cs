9479d8f6f6fdd509dadf4c825f77294ea25e8049
Marco Alayn <marc_rob92@hotmail.com>
2024-09-01 19:33:57 -0700

    Agregar archivos de proyecto.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,52 @@
+﻿using CrudStudentsApi.Data;
+using CrudStudentsApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudStudentsApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private readonly string _connectionString;
+
+        public StudentController(AppDbContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        //GetAllStudents
+        [HttpGet]
+        public async Task<IActionResult> GetAllStudents()
+        {
+            var response = new ApiResponse<List<Student>>();
+
+            try
+            {
+                var students = await _context.Students.FromSqlRaw("EXEC sp_student @opt = 1").ToListAsync();
+
+                response.Success = true;
+                response.Message = "Lista de estudiantes obtenida con éxito.";
+                response.Data = students;
+
+                return Ok(response);
+            }
+            catch (SqlException ex)
+            {
+                response.Success = false;
+                response.Message = $"Error en la base de datos: {ex.Message}";
+                return StatusCode(500, response);
+            }
+            catch (Exception ex)
+            {
+                response.Success = false;
+                response.Message = $"Error interno del servidor: {ex.Message}";
+                return StatusCode(500, response);
+            }
+        }
+    }
+}
---
+++ Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using CrudStudentsApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudStudentsApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Student> Students { get; set; }
+    }
+}
